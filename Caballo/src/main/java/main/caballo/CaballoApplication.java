54cc4fa9fa971b395861aa844d963d12a936e9fe
adamirnukic <adamir.nukic.23@size.ba>
2025-12-14 19:02:04 +0100

    Inicijalna struktura projekta

---
+++ Caballo/src/main/java/main/caballo/CaballoApplication.java
@@ -0,0 +1,19 @@
+package main.caballo;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class CaballoApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(CaballoApplication.class.getResource("/main/caballo/view/login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 900, 600);
+        stage.setTitle("Caballo");
+        stage.setScene(scene);
+        stage.show();
+    }
+}

d786d0c9c95a00d5f4f9e01dcc3d8648d2b4e44d
adamirnukic <162193116+adamirnukic@users.noreply.github.com>
2026-02-06 00:43:18 +0100

    Prelazak sa lokalne baze na Aiven cloud bazu

--- Caballo/src/main/java/main/caballo/util/DbUtil.java
+++ Caballo/src/main/java/main/caballo/util/DbUtil.java
@@ -7,10 +7,10 @@ import java.sql.SQLException;
 public final class DbUtil {
     private static final String URL = System.getenv().getOrDefault(
             "DB_URL",
-            "jdbc:mysql://localhost:3306/caballo?useSSL=false&allowPublicKeyRetrieval=true&autoReconnect=true&useUnicode=true&characterEncoding=UTF-8"
+            "jdbc:mysql://avnadmin:AVNS_CKrPgbVfJGrItEt9POo@caballo-mysql-size-mrs.d.aivencloud.com:10451/caballo?ssl-mode=REQUIRED&allowPublicKeyRetrieval=true&autoReconnect=true&useUnicode=true&characterEncoding=UTF-8"
     );
-    private static final String USER = System.getenv().getOrDefault("DB_USER", "root");
-    private static final String PASS = System.getenv().getOrDefault("DB_PASS", "root");
+    private static final String USER = System.getenv().getOrDefault("DB_USER", "avnadmin");
+    private static final String PASS = System.getenv().getOrDefault("DB_PASS", "AVNS_CKrPgbVfJGrItEt9POo");
 
     private DbUtil() {}
 
